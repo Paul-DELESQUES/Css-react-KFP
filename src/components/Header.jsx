a5bb4318b143d2a1400a381fc540c92865a0441d
Paul-DELESQUES <pldelesques+github@gmail.com>
2023-10-17 12:16:13 +0200

    create cards ninjaturtles

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,11 +1,11 @@
 import PropTypes from "prop-types";
 
 const Header = (props) => {
-    const { title, title2 } = props;
+    const { title, title2, changeTheme, changePreviousTheme } = props;
     return (
         <header>
-            <h2 className="title">{title}</h2>
-            <h2 className="title2">{title2}</h2>
+            <h2 className="title" onClick={() => changePreviousTheme()}>{title}</h2>
+            <h2 className="title2" onClick={() => changeTheme()}>{title2}</h2>
         </header>
     );
 }
---
+++ src/components/datas/dataNT.jsx
@@ -0,0 +1,29 @@
+const dataNinjaturtles = [
+    {
+        imgSrc: "https://media.giphy.com/media/l4Ki4wh8V4si0Ypbi/giphy.gif",
+        name: "Benoît Le GOAT",
+        type: "Gif-1",
+    },
+    {
+        imgSrc: "https://media.giphy.com/media/xT0BKF1QhKdVaKHkru/giphy.gif",
+        name: "Leonardo",
+        type: "Gif-2",
+    },
+    {
+        imgSrc: "https://media.giphy.com/media/d4bmpRK7MQIZy1nG/giphy.gif",
+        name: "Donatello",
+        type: "Gif-3",
+    },
+    {
+        imgSrc: "https://media.giphy.com/media/xT0BKiaacPmZBdoCYM/giphy.gif",
+        name: "Michelangelo The Best",
+        type: "Gif-4",
+    },
+    {
+        imgSrc: "https://media.giphy.com/media/hXIcxpC8Ycl8A1mLOC/giphy.gif",
+        name: "The Dream Team",
+        type: "Gif-5",
+    },
+];
+
+export default dataNinjaturtles;
